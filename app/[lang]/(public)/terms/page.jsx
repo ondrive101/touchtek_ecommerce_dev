786f9fd81398b2b9acd06908c3dffe7d06458a49
onedrive101 <ondrive.101@gmail.com>
2025-12-12 13:00:24 +0530

    bug

--- app/[lang]/(public)/terms/page.jsx
+++ app/[lang]/(public)/terms/page.jsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { useState } from 'react';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
+import Header from "@/components/layout/components/Header";
+import Footer from "@/components/layout/components/Footer";
 import { motion } from 'framer-motion';
 import { 
   FileText, Shield, Users, CreditCard, Package, 
